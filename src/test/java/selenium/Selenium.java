7a3215a108633473b72261876695dae97c721f59
Natalia Rychlowska <natalia.rychlowska@jeppensen.com>
2021-11-09 13:16:02 +0100

    selenium test

---
+++ src/test/java/selenium/Selenium.java
@@ -0,0 +1,34 @@
+package selenium;
+
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+
+public class Selenium {
+    WebDriver driver;
+    private ChromeOptions initialize() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("logLevel", "INFO");
+        options.setCapability("browserAttachTimeout", 1000);
+        return options;
+    }
+    public WebDriver getDriver() {
+        ChromeOptions options = initialize();
+            driver = new ChromeDriver(options);
+        return driver;
+    }
+
+    @Test
+    public void seleniumTest() {
+        getDriver();
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("test");
+    }
+
+}
